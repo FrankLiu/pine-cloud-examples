46c3ba902b1e4a0a5e992efbb2d8345255d21db4
hzliuj <hzliuj@tairanchina.com>
2018-12-20 18:24:07 +0800

    upgrade spring-cloud to 2.0.6.RELEASE version

--- pine-cloud-api-gateway/src/main/java/org/pine/cloud/api/gateway/ApiGatewayApplication.java
+++ pine-cloud-api-gateway/src/main/java/org/pine/cloud/api/gateway/ApiGatewayApplication.java
@@ -3,10 +3,10 @@ package org.pine.cloud.api.gateway;
 import org.pine.cloud.api.gateway.auth.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-@EnableZuulProxy
 @SpringCloudApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
@@ -17,4 +17,11 @@ public class ApiGatewayApplication {
 	public AccessFilter accessFilter() {
 		return new AccessFilter();
 	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
 }
+
+// refer https://spring.io/guides/gs/gateway/
--- pine-cloud-computer-service/src/main/java/org/pine/cloud/web/ComputerController.java
+++ pine-cloud-computer-service/src/main/java/org/pine/cloud/web/ComputerController.java
@@ -2,27 +2,18 @@ package org.pine.cloud.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComputerController {
 	private static final Logger logger = LoggerFactory.getLogger(ComputerController.class);
-	
-	@Autowired
-	private DiscoveryClient client;
-
-	@Value("${service-name}")
-	private String serviceName;
 
 	@GetMapping("/add")
 	public Integer add(@RequestParam Integer a, @RequestParam Integer b){
-		ServiceInstance instance = client.getInstances("compute-service").get(0);
 		Integer r = a + b;
-		logger.info("/add, host: {}, service_id: {}, service_name: {}, result: {}", instance.getHost(), instance.getServiceId(), serviceName, r);
+		logger.info("/add, params: {}, result: {}", a, b, r);
 		return r;
 	}
 }
