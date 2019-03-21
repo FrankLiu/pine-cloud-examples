1d9083d13c913cb2f31d6506bcbe65c76e804068
Frank Liu <frankliu@github.comm>
2019-03-21 21:13:17 +0800

    add Resp model

--- services/user-service/src/main/java/io/pine/cloud/service/user/interfaces/UserController.java
+++ services/user-service/src/main/java/io/pine/cloud/service/user/interfaces/UserController.java
@@ -2,6 +2,7 @@ package io.pine.cloud.service.user.interfaces;
 
 import io.pine.cloud.service.user.application.UserService;
 import io.pine.cloud.service.user.domain.User;
+import io.pine.cloud.service.user.interfaces.vo.Resp;
 import io.pine.cloud.service.user.interfaces.vo.UserVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,13 @@ public class UserController {
     @ApiOperation(value="创建用户", notes="根据User对象创建用户", produces = "application/json")
     @ResponseBody
     @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public User newUser(@ModelAttribute(name = "user") UserVo userVo) {
+    public Resp<User> newUser(@ModelAttribute(name = "user") UserVo userVo) {
         User user = new User();
         user.setName(userVo.getUsername());
         user.setPassword(userVo.getPasswd());
         user.setAge(userVo.getAge());
         user.setEmail(userVo.getEmail());
-        return userService.createUser(user);
+        return Resp.success( "SUCCESS", userService.createUser(user));
     }
 
     @ApiOperation(value = "获取用户列表", notes = "获取用户列表", produces = "application/json")
--- services/user-service/src/main/java/io/pine/cloud/service/user/interfaces/vo/Resp.java
+++ services/user-service/src/main/java/io/pine/cloud/service/user/interfaces/vo/Resp.java
@@ -17,9 +17,16 @@ public class Resp<T> {
     public static <T> Resp of(int code, String message, T body) {
         return new Resp(code, message, body);
     }
+    public static <T> Resp of(Code code, T body) {
+        return of(code.getCode(), code.getDescription(), body);
+    }
+
+    public static <T> Resp success(String message, T body) {
+        return of(200, message, body);
+    }
 
     public static <T> Resp success(T body) {
-        return of(200, "OK", body);
+        return success("OK", body);
     }
 
     public static <T> Resp fail(int code, String message, T body) {
@@ -28,5 +35,26 @@ public class Resp<T> {
 
     public static <T> Resp fail(int code, T body) {
         return fail(code, "Failed", body);
+    }
+
+    public enum Code {
+        SUCCESS(200, "OK"),
+        CLIENT_ERROR(400, "Invalid Request"),
+        SERVER_ERROR(500, "Internal Error");
+
+        private int code;
+        private String description;
+
+        Code(int code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public int getCode() {
+            return this.code;
+        }
+        public String getDescription() {
+            return this.description;
+        }
     }
 }
