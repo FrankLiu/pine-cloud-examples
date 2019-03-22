a5067036c04e6506cd440c4bef0d3e5da5b417ed
Frank Liu <frankliu@github.comm>
2019-03-22 22:59:44 +0800

    add ecard-service based on mybatis

---
+++ services/ecard-service/src/main/java/io/pine/cloud/examples/ecard/infrastructure/UserMapper.java
@@ -0,0 +1,8 @@
+package io.pine.cloud.examples.ecard.infrastructure;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.pine.cloud.examples.ecard.domain.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
